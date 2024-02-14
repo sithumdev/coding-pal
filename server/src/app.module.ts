fe274c80c50043bbbf176cb438a1f6e2ada92b7a
Sithum Basnayaka <sithum.dashantha@gmail.com>
2024-02-14 17:07:03 +0530

    mongodb URL changed

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ParticipantModule } from './participant/participant.module';
 import { RoomModule } from './room/room.module';
 import { ChatModule } from './chat/chat.module';
@@ -10,9 +10,13 @@ import { ChatModule } from './chat/chat.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(
-      'mongodb+srv://sithumdashantha:x0T3fVsjz7DBBdsz@coding-pal.sxcpnkx.mongodb.net/',
-    ),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URL'),
+      }),
+    }),
     ParticipantModule,
     RoomModule,
     ChatModule,
